99826a262d7809d9deb4f921509536fe5c78451d
Gustavo Ramalho <gugarroliver@hotmail.com>
2018-12-11 00:35:44 -0200

    adicionar ativo e eventos

--- src/js/index.js
+++ src/js/index.js
@@ -25,32 +25,39 @@ const manager = {
 
 	remove : function(id){
 		this.ativos = this.ativos.filter(ativo => ativo.id != id);
+	},
+
+	update : function(id, value){
+		this.ativos = this.ativos.map(ativo => (ativo.id == id) ? {...ativo, value} : ativo);
 	}
 }
 
-function generateRandomAtivo(){
 
-	const names = ["sparta", "ciclico", "fi", "multimercado", "top", "previdencia", 
-								 "advisory", "icatu", "renda", "fixa", "crédito", "privado", 
-								 "petrobras", "pn"];
-									
+const names = ["sparta", "ciclico", "fi", "multimercado", "top", "previdencia", 
+							 "advisory", "icatu", "renda", "fixa", "crédito", "privado", 
+							 "petrobras", "pn"];
+							 
+function randomName(){
+	let name = "";
+	let c = Math.floor(Math.random() * 4) + 1;
+	
+	for (let i = 0; i < c; i++){
+		name += names[Math.floor(Math.random() * names.length)];
+		name += (i != c-1) ? " " : "";
+	}
+
+	return name;
+}
+
+function randomValue(){
+	return parseFloat((Math.random() * 10000 + 100).toFixed(2));
+}
+
+
+function generateRandomAtivo(){
+				
 	return new Ativo(0, randomName(), randomValue());
 
-	function randomName(){
-		let name = "";
-		let c = Math.floor(Math.random() * 4) + 1;
-		
-		for (let i = 0; i < c; i++){
-			name += names[Math.floor(Math.random() * names.length)];
-			name += (i != c-1) ? " " : "";
-		}
-	
-		return name;
-	}
-
-	function randomValue(){
-		return parseFloat((Math.random() * 10000 + 100).toFixed(2));
-	}
 }
 
 
@@ -58,7 +65,7 @@ function createAtivoElement(ativo){
 	
 	let tr = document.createElement('tr');
 
-	let checkBox = document.createElement("checkbox");
+	let checkBox = document.createElement("input");
 	checkBox.setAttribute('type', 'checkbox');
 
 	let tdCheckBox = document.createElement('td');
@@ -72,6 +79,7 @@ function createAtivoElement(ativo){
 	let inputValue = document.createElement('input');
 	inputValue.setAttribute('id', 'value' + ativo.id);
 	inputValue.setAttribute('type', 'number');
+	inputValue.setAttribute('value', ativo.value);
 
 	let tdValue = document.createElement('td');
 	tdValue.appendChild(document.createTextNode("R$"));
@@ -101,7 +109,14 @@ function createAtivoElement(ativo){
 
 	return tr;
 }
- 
+
+
+
+const changeTotalEvent = () => {
+
+}
+
+
 function main(){
 
 	for (let i=0; i<3; i++){
@@ -109,8 +124,48 @@ function main(){
 		console.log(manager.ativos);
 	}
 
+	let total = manager.total();
+	document.querySelector('#total').value =  total;
+	document.querySelector('#porcentagem_total').innerHTML = "100 %";
+	document.querySelector('#restante').innerHTML = '(Restante: 0,00)';
+	document.querySelector('#qtsAtivos').innerHTML = `(${manager.ativos.length})`;
 
-	manager.ativos.map(ativo => document.querySelector("#table_ativos").appendChild(createAtivoElement(ativo)));
+
+	manager.ativos.map(ativo => { 
+		document.querySelector('#table_ativos').appendChild(createAtivoElement(ativo));
+		document.querySelector('#percent' + ativo.id).value = (ativo.value / total * 100).toFixed(2);
+
+		document.querySelector('#value' + ativo.id).addEventListener("change", e => {
+			const id = e.target.id.split('value')[1];
+			manager.update(id, e.target.value);
+			let restante = document.querySelector("#total").value - manager.total();
+			document.querySelector('#restante').innerHTML = `(Restante: ${restante.toFixed(2)})`;	
+
+		})
+ 
+	});
+
+	document.querySelector("#btnAdd").addEventListener("click", () => {
+
+		const ativo = new Ativo(0, randomName(), 0);
+		manager.add(ativo);
+		document.querySelector("#table_ativos").appendChild(createAtivoElement(ativo));
+		document.querySelector('#percent' + ativo.id).value = (ativo.value / manager.total() * 100).toFixed(2);
+	});
+
+
+	const inputTotal = document.querySelector('#total');
+
+	inputTotal.addEventListener("change", event => { 
+			let restante = event.target.value - total;
+			document.querySelector('#restante').innerHTML = `(Restante: ${restante.toFixed(2)})`;	
+	});
+
+
+
+
+
+
 }
 
 main();
